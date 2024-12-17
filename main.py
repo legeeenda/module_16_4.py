995cd719a14f9b19e31da6dde63d1d85a4447924
unknown <you@example.com>
2024-12-17 19:32:32 +0500

    first committt

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
-# Инициализация FastAPI
+
 app = FastAPI()
 
-# Список пользователей
+
 users = []
 
-# Модель пользователя
+
 class User(BaseModel):
     id: int
     username: str
     age: int
 
-# GET запрос - возвращает список всех пользователей
+
 @app.get("/users")
 def get_users():
     return users
 
-# POST запрос - добавляет нового пользователя
+
 @app.post("/user/{username}/{age}")
 def create_user(username: str, age: int):
     new_id = users[-1].id + 1 if users else 1
@@ -26,7 +26,7 @@ def create_user(username: str, age: int):
     users.append(user)
     return user
 
-# PUT запрос - обновляет существующего пользователя по его id
+
 @app.put("/user/{user_id}/{username}/{age}")
 def update_user(user_id: int, username: str, age: int):
     for user in users:
@@ -36,7 +36,7 @@ def update_user(user_id: int, username: str, age: int):
             return user
     raise HTTPException(status_code=404, detail="User was not found")
 
-# DELETE запрос - удаляет пользователя по его id
+
 @app.delete("/user/{user_id}")
 def delete_user(user_id: int):
     for i, user in enumerate(users):
